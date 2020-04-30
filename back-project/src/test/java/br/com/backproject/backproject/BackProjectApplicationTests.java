4fa921b45604b6e80fc66fe4c1e1bd3ea8ca6bd8
Victoorres <victoorres@icloud.com>
2020-04-30 17:04:04 -0300

    Commit initial

---
+++ back-project/src/test/java/br/com/backproject/backproject/BackProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.backproject.backproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
